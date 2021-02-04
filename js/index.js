f718e9636632a39f26f2102a70cd8188efb0c129
dewarian <nathanbommezijn@gmail.com>
2021-02-04 22:45:47 +0100

    :sparkles: Insert crew name into DOM

--- js/index.js
+++ js/index.js
@@ -1,24 +1,31 @@
-/* 
-  I am searching for a job, hire me
-  https://www.github.com/dewarian
-*/
-
 const baseUrl = 'https://api.spacexdata.com/v4';
+const endpoint = 'crew'
+const mainContainer = document.querySelector('main');
 
 // Insert current year in footer
 document.querySelector('footer span').insertAdjacentHTML('afterbegin', new Date().getFullYear());
 
+
 const getData = async (url) => {
   return await fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data))
+  // .then(data => console.log(data))
+  .then(data => { render(data);})
+}
+
+function render(data) {
+   Object.values(data).map(
+    key => {
+      return mainContainer.insertAdjacentHTML('beforeend', `<h1>${key.name}</h1>`)
+    }
+  )
 }
 
 /**
  * @description immediately self invoking function javascript, executes when block is called.
- * 
  */
 (async function(){
-  console.log('hello');
-  await getData(`${baseUrl}/ships`)
+  // await console.log(getData(`${baseUrl}/${endpoint}`).then(data => console.log(data)))
+  // await render(getData(`${baseUrl}/${endpoint}`))
+  getData(`${baseUrl}/${endpoint}`)
 })();
