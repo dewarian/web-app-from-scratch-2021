b7a05603dfedab0950032f8cfa14410ca5e6e582
dewarian <nathanbommezijn@gmail.com>
2021-02-04 09:57:57 +0100

    :boom: Breaking change, making a template enginen

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
+/* 
+  I am searching for a job, hire me
+  https://www.github.com/dewarian
+*/
+
+const baseUrl = 'https://api.spacexdata.com/v4';
+
 // Insert current year in footer
-document.querySelector('footer span').insertAdjacentHTML('afterbegin', new Date().getFullYear())
+document.querySelector('footer span').insertAdjacentHTML('afterbegin', new Date().getFullYear());
+
+const getData = async (url) => {
+  return await fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
+/**
+ * @description immediately self invoking function javascript, executes when block is called.
+ * 
+ */
+(function(){
+  console.log('hello');
+  getData(`${baseUrl}/ships`)
+})();
