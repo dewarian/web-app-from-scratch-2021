f3299edb8bd40350215392be1391f2fef415ebf1
dewarian <nathanbommezijn@gmail.com>
2021-02-09 17:09:45 +0100

    :memo: update documentation for getData

--- static/js/modules/data.js
+++ static/js/modules/data.js
@@ -7,7 +7,8 @@ export const datatest = (test) => {
   return console.log(`%c${test}`, 'color: #ffcc00; font-weight: bold;')
 }
 /**
- * @description Retrieves data and runs data dependent functionality
+ * @title Retrieve Data from API
+ * @description Data retrieval. Use getData when retrieving data from an external API.
  * @param {String} url API endpoint to call.
  */
 export const getData = async (url) => {
