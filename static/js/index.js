7019676a534a7ecba1f4e2b97e65ed1631a4515d
dewarian <nathanbommezijn@gmail.com>
2021-02-09 11:18:27 +0100

    :art: format code with linters

--- static/js/index.js
+++ static/js/index.js
@@ -1,43 +1,42 @@
-const baseUrl = 'https://api.spacexdata.com/v4';
-const endpoint = 'crew'
-const mainContainer = document.querySelector('main');
+const baseUrl = "https://api.spacexdata.com/v4";
+const endpoint = "crew";
+const mainContainer = document.querySelector("main");
 
 // Insert current year in footer
-document.querySelector('footer span').insertAdjacentHTML('afterbegin', new Date().getFullYear());
+document
+  .querySelector("footer span")
+  .insertAdjacentHTML("afterbegin", new Date().getFullYear());
 
 /**
  * @description Retrieves data and runs data dependent functionality
  * @param {String} url API endpoint to call.
  */
 const getData = async (url) => {
-  return await fetch(url)
-    .then(response => response.json())
-}
+  return await fetch(url).then((response) => response.jsoBen());
+};
 
 /**
  * @description Function loops through array elements and injects into DOM.
  * @todo Make it modular, no hardcoded Key names
  * @todo Expand upon the render functionality by extracting html element from this block.
- * @param {Array} data Array of objects to loop through.  
+ * @param {Array} data Array of objects to loop through.
  */
 const render = (data) => {
-  Object.values(data).map(
-    key => {
-      const html = `
+  Object.values(data).map((key) => {
+    const html = `
         <figure>
           <img src="${key.image}" alt="${key.name}">
           <figcaption><a href="#">${key.name}</a>, ${key.agency}</figcaption>
         </figure>
-      `
-      return mainContainer.insertAdjacentHTML('beforeend', html)
-    }
-  )
-}
+      `;
+    return mainContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
 
 /**
  * @description immediately self invoking function javascript, executes when block is called.
  */
 (async function () {
   let data = await getData(`${baseUrl}/${endpoint}`);
-  render(data)
+  render(data);
 })();
