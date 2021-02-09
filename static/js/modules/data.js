75527fa1fc6075338795a3620224be1fed56e11b
dewarian <nathanbommezijn@gmail.com>
2021-02-09 16:18:03 +0100

    :zap: :poop: Create block scopes.
    Moving code blocks in their own scopes without any access to the global scope.
    
    Moved from .insertAdjacentHTML to .createElement for performance and security reasons.

--- static/js/modules/data.js
+++ static/js/modules/data.js
@@ -1,3 +1,10 @@
 export const datatest = (test) => {
-  return console.log(test)
+  return console.log(`%c${test}`, 'color: #ffcc00; font-weight: bold;')
+}
+/**
+ * @description Retrieves data and runs data dependent functionality
+ * @param {String} url API endpoint to call.
+ */
+export const getData = async (url) => {
+  return await fetch(url).then((response) => response.json())
 }
---
+++ static/js/modules/render.js
@@ -0,0 +1,19 @@
+/**
+ * @description Function loops through array elements and injects into DOM.
+ * @todo Make it modular, no hardcoded Key names
+ * @todo Expand upon the render functionality by extracting html element from this block.
+ * @param {Array} data Array of objects to loop through.
+ */
+export const render = (data) => {
+  const mainContainer = document.querySelector('main')
+
+  Object.values(data).map((key) => {
+    const html = `
+        <figure>
+          <img src="${key.image}" alt="${key.name}">
+          <figcaption><a href="#">${key.name}</a>, ${key.agency}</figcaption>
+        </figure>
+      `
+    return mainContainer.insertAdjacentHTML('beforeend', html)
+  })
+}
